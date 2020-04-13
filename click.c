07320de8576b4db22f5b0e0e42faa385290efbbf
Ray Garner <ray@perfectcast.co.uk>
2020-04-13 17:07:34 +0100

    more code cleanup

--- click.c
+++ click.c
@@ -6,10 +6,17 @@
 #include <string.h>
 
 #define ONESEC 1000000
+#define ONEBPM 60000000
 #define CLICK  "beep.wav &"
 #define PLAYER "mpv "
 
+double CalcInterval(int bpm);
 int play();
+
+double CalcInterval(int bpm)
+{
+    return ONEBPM / bpm;
+}
 
 int play()
 {
@@ -21,13 +28,7 @@ int play()
 
 int main(int argc, char **argv)
 {
-    int bpm = atoi(argv[1]);
-    double onebpm = ONESEC * 60;
-    double interval = onebpm / bpm;
-
-    printf("bpm = %d\n", bpm);
-    printf("onebpm = %f\n", onebpm);
-    printf("interval = %f\n", interval);
+    double interval = CalcInterval(atoi(argv[argc - 1]));
 
     while (1) {
         play();
